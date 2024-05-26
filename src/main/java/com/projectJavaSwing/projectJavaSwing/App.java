a773558c2bab6d89a9b5a191342e582d13143ab1
DangManh00 <dangmanh010801@gmail.com>
2024-05-26 14:51:31 +0700

    Initial commit

---
+++ src/main/java/com/projectJavaSwing/projectJavaSwing/App.java
@@ -0,0 +1,13 @@
+package com.projectJavaSwing.projectJavaSwing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
